288f91ca9e1a1697f2f9f4d3e24ad40245dac704
Mr_Choco <chocoretostonus@gmail.com>
2023-10-02 01:36:30 -0600

    Capsules Reader

--- Assets/Project/Scripts/DB/CDFM.cs
+++ Assets/Project/Scripts/DB/CDFM.cs
@@ -8,8 +8,6 @@ using Object = System.Object;
 
 public class CDFM 
 {
-
-
     #region Variables
 
     public List<Object>[] Lists_Obj;
@@ -19,20 +17,10 @@ public class CDFM
     
     #endregion
 
-    
-    
     #region MyMethods
-
 
     #region DataSaver
     
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="MainFolder">Name of the MainSaver Data Folder</param>
-    /// <param name="Subfolders">Name of the folders that contains Data</param>
-    /// <returns></returns>
     public void MainSaver(string MainFolder, string[] Subfolders, List<Object>[] lst_Lists_Obj)
     {
         FolderPaths(MainFolder,Subfolders);
@@ -52,13 +40,13 @@ public class CDFM
     
     void FolderPaths(string MainFol, string[] SubFol = default)
     {
-        MainPath = MainFol == "" ? $"{Application.dataPath}/{MainPath}" : $"{Application.dataPath}/DataBase";
-        string[] NewPaths(string[] SubFoli){
-            for (var i = 0; i < SubFoli.Length; i++)
-                SubPaths[i] = $"{MainPath}/{SubFoli[i]}";
-            return SubFoli;}
+        MainPath = MainFol == "" ? $"{Application.persistentDataPath}/{MainPath}" : $"{Application.persistentDataPath}/DataBase";
+        Func<string[],string[]> NewPaths = (a) =>{
+            for (var i = 0; i < a.Length; i++)
+                SubPaths[i] = $"{MainPath}/{a[i]}";
+            return a;
+        };
         SubPaths = SubFol == default ? NewPaths(SubFol) : new string[] { $"{MainPath}/Anime", $"{MainPath}/Manga"};
-
     }
 
 
@@ -98,7 +86,6 @@ public class CDFM
                         }
                         CapsuleDirectory(SimplePath, $"{w.GetType().Name}_{counte}", w);
                         counte++;
-                        Debug.Log(w.GetType().Name);
                     }
                 }
                 
@@ -122,26 +109,61 @@ public class CDFM
     
     #endregion
     
-    
-    
     #region DataLoader
+
+    public DataBase MainLoader(string MainFolder, string[] Subfolders)
+    {
+        DataBase cls_BD = new();
+        FolderPaths(MainFolder,Subfolders);
+        cls_BD = LoadCapsule();
+        return cls_BD;
+    }
     
-    
-    
-    
+    public DataBase MainLoader(string[] Subfolders)
+    {
+        DataBase cls_BD = new();
+        string MainFolder = default;
+        FolderPaths(MainFolder,Subfolders);
+        cls_BD = LoadCapsule();
+        return cls_BD;
+    }
+
+    DataBase LoadCapsule()
+    {
+        DataBase cls_BD = new();
+        string str_Json = default;
+        string[] Files = default;
+        StreamReader streamReader;
+        
+        if (Directory.Exists(MainPath))
+            foreach (var subPath in SubPaths)
+            {
+                if (!File.Exists(subPath))
+                {
+                    Files = Directory.GetFiles(subPath);
+
+                    if (subPath.Contains("Anime"))
+                        foreach (var file in Files){
+                            streamReader = new(file);
+                            str_Json = streamReader.ReadToEnd();
+                            cls_BD.AnimeList.Add(JsonUtility.FromJson<Anime>(str_Json));
+                            streamReader.Close();
+                        }
+                    
+                    else if (subPath.Contains("Manga"))
+                        foreach (var file in Files){
+                            streamReader = new(file);
+                            str_Json = streamReader.ReadToEnd();
+                            cls_BD.MangaList.Add(JsonUtility.FromJson<Manga>(str_Json));
+                            streamReader.Close();
+                        }
+                }
+            }
+        return cls_BD;
+    }
+
+    #endregion
     
     #endregion
-
-    
-    #endregion
-
-
-
-    #region Corrutines
-
-
-
-    #endregion
-
 
 }
--- Assets/Project/Scripts/DB/FDS.cs
+++ Assets/Project/Scripts/DB/FDS.cs
@@ -10,9 +10,7 @@ public class FDS : MonoBehaviour
 
     #region Variables
 
-    [HideInInspector] public string stg_DOKDB_Json; //DOKDB = Default OKaimono DataBase
-    [HideInInspector] public string stg_Application_Path; //Path de la carpeta donde se guardara el archivo
-    //[HideInInspector] public bool bolChanges;
+    [HideInInspector] const string stg_App_Path = "/Okaimono/"; //Path de la carpeta donde se guardara el archivo
     const string stgDOKDBF = "DOKDB.dcuf"; //DOKDBF = Default OKaimono DataBase File
                                           //.dcuf = Dot Choco Universal File
     DataBase cls_BD; //BD = DataBase
@@ -28,25 +26,13 @@ public class FDS : MonoBehaviour
 
     void Awake()
     {
-        cls_DCFP = new();
-        cls_BD = new();
         cls_CDFM = new();
+        cls_BD = new();
     }
-
-
 
     void Start()
     {
-        stg_Application_Path = Application.persistentDataPath + "/Okaimono/";
-        // cls_DCFP = new();
-        //Se le da una path a la variable stgApplication_Path desde el Start
-        //ya que de este modo me aseguro de tener la ruta correcta y no tener
-        //problemas con la carga de datos
-        stg_Application_Path = Application.persistentDataPath + "/Okaimono/";
-        //Debug.Log(stgApplication_Path);
-        //CreateFile();
-        SaveData();
-        // LoadData();
+        COLD();
     }
 
 
@@ -54,43 +40,18 @@ public class FDS : MonoBehaviour
 
 
 
-    //MDB(Manage DataBase)
+    //MDB(Manage_Data_Base)
     #region Methods_of_MDB
 
     [ContextMenu("SaveData")]
     public void SaveData()
     {
-        Anime anime = new();
-        Anime anim = new();
-        Anime animew = new();
-        anime.Name = "Naruto";
-        anim.Name = "Bleach";
-        animew.Name = "One Piece";
-        cls_CDFM = new();
-        cls_BD = new();
-        cls_BD.AnimeList.Add(anime);
-        cls_BD.AnimeList.Add(anim);
-        cls_BD.AnimeList.Add(animew);
-        cls_BD.MangaList.Add(new());
-        cls_BD.MangaList.Add(new());
-        
-        
         cls_CDFM.Lists_Obj = new List<object>[2];
         cls_CDFM.Lists_Obj[0] = new List<object>(){cls_BD.AnimeList};
         cls_CDFM.Lists_Obj[1] = new List<object>(){cls_BD.MangaList};
         string[] Subfolders = new string[] { "Anime", "Manga" };
         
         cls_CDFM.MainSaver(Subfolders, cls_CDFM.Lists_Obj);
-        
-        
-        
-        
-        // //le pasamos los datos de la clsBD al stgDOKDB_JsonObj
-        // stg_DOKDB_Json = cls_DCFP.ToString(cls_BD); //DCFP = Dot Choco File Parse
-        // // JsonUtility.ToJson(stg_DOKDB_Json);
-        //guarda los datos de la clsBD publica en el Archivo "stgDOKDB_JsonObj" creado
-        // File.WriteAllText(stg_Application_Path + stgDOKDBF, stg_DOKDB_Json);
-        // Debug.Log(stg_DOKDB_Json);
     }
     
     
@@ -98,76 +59,24 @@ public class FDS : MonoBehaviour
     [ContextMenu("LoadData")]
     public void LoadData()
     {
-        // DataBase cls_BDs = new();
-        //busca el archivo en la ruta que le pasamos
-        // stg_DOKDB_Json = File.ReadAllText(stg_Application_Path + stgDOKDBF);
-        //y aqui le pasa los valores a la clsBD publica 
-        // cls_BDs = DCFP.ToClass<DataBase>(stg_DOKDB_Json); //DCFP = Dot Choco File Parse
-        // clsBD = JsonUtility.FromJson<DataBase>(stgDOKDB_JsonObj);
-        
-        
-        
-        
+        string[] Subfolders = new string[] { "Anime", "Manga" };
+        cls_BD =  cls_CDFM.MainLoader(Subfolders);
     }
 
 
-
-    public void CreateFile()
+    //COLD = Create Or Load Data
+    void COLD()
     {
-        //Aqui ponemos el nombre del Archivo junto a su extension, en mi caso por facilidad y comprension
-        //a�adi la variable donde esta la ruta junto al nombre del archivo 
-        string DOKDB = stg_Application_Path + stgDOKDBF;
-
-        //Si existe la carpeta pero el archivo no, solo creara el json
-        //y a su vez borrara el Highscore que hayamos tenido
-        if (File.Exists(stg_Application_Path) && !File.Exists(DOKDB))
-        {
-            PlayerPrefs.DeleteKey("HighScore");
-
-            stg_DOKDB_Json = JsonUtility.ToJson(cls_BD);
-            File.WriteAllText(DOKDB, stg_DOKDB_Json);
-            Debug.Log("json create");
-        }
-
-        //Si no existe la carpeta ni el archivo creara ambos
-        //y a su vez borrara el Highscore que hayamos tenido
-        else if (!File.Exists(stg_Application_Path) && !File.Exists(DOKDB))
-        {
-            PlayerPrefs.DeleteKey("HighScore");
-
-            Directory.CreateDirectory(stg_Application_Path);
-            stg_DOKDB_Json = JsonUtility.ToJson(cls_BD);
-            File.WriteAllText(DOKDB, stg_DOKDB_Json);
-
-            stg_DOKDB_Json = JsonUtility.ToJson(cls_BD);
-            File.WriteAllText(DOKDB, stg_DOKDB_Json);
-            Debug.Log("json create & folder create");
-        }
-
-        //Si existen ambos solo cargara los datos
-        else if (File.Exists(stg_Application_Path) && File.Exists(DOKDB))
-        {
+        //Si no existe la carpeta de la aplicacion la creara y guardara los datos
+        if (!Directory.Exists(Application.persistentDataPath + stg_App_Path))
+            SaveData();
+        //Caso contrario solo cargara los datos
+        else 
             LoadData();
-            Debug.Log("json load");
-        }
-
-
-        //-------------------------------------------------------------------------------------------
-
-
     }
 
 
     #endregion
 
-
-
-    #region Corrutines
-
-
-    #endregion
-
-
-    
 
 }
